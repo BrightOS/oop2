a9551fa3a7f367f0a68296b71553bb1c2e3931e7
BrightOS <dbarov3@gmail.com>
2022-10-31 20:41:45 +0500

    reformating output

--- main.cpp
+++ main.cpp
@@ -30,19 +30,23 @@ private:
 
 public:
     Matrix() : width(800), height(600) {
-        std::cout << "Matrix -> Default constructor was called" << std::endl;
+        std::cout << "Matrix()" << std::endl;
     }
 
     explicit Matrix(int dim) : width(dim), height(dim) {
-        std::cout << "Matrix -> Constructor with dim parameter was called" << std::endl;
+        std::cout << "Matrix(int dim)" << std::endl;
     }
 
     Matrix(int width, int height) : width(width), height(height) {
-        std::cout << "Matrix -> Constructor with screen_width and screen_height parameters was called" << std::endl;
+        std::cout << "Matrix(int width, int height)" << std::endl;
+    }
+
+    Matrix(const Matrix &matrix) : width(matrix.width), height(matrix.height) {
+        std::cout << "Matrix(const Matrix &matrix)" << std::endl;
     }
 
     ~Matrix() {
-        std::cout << "Matrix -> The destructor was called" << std::endl;
+        std::cout << "~Matrix()" << std::endl;
     }
 
     int get_width() const {
@@ -69,26 +73,26 @@ protected:
 public:
     Screen() : Matrix() {
         init_pixels();
-        std::cout << "Screen -> Default constructor was called" << std::endl;
+        std::cout << "Screen()" << std::endl;
     }
 
     explicit Screen(int dim) : Matrix(dim) {
         init_pixels();
-        std::cout << "Screen -> Constructor with dim parameter was called" << std::endl;
+        std::cout << "Screen(int dim)" << std::endl;
     }
 
     Screen(int screen_width, int screen_height) : Matrix(screen_width, screen_height) {
         init_pixels();
-        std::cout << "Screen -> Constructor with screen_width and screen_height parameters was called" << std::endl;
+        std::cout << "Screen(int screen_width, int screen_height)" << std::endl;
     }
 
     Screen(const Screen &screen) : Matrix(screen.get_width(), screen.get_height()),
                                    pixels(screen.pixels) {
-        std::cout << "Screen -> The copy constructor was called" << std::endl;
+        std::cout << "Screen(const Screen &screen)" << std::endl;
     }
 
     ~Screen() {
-        std::cout << "Screen -> The destructor was called" << std::endl;
+        std::cout << "~Screen()" << std::endl;
     }
 
     void set_pixel(int width, int height, int color) {
@@ -101,6 +105,40 @@ public:
 };
 
 
+class Camera : public Matrix {
+private:
+    float pixel_size; // в дюймах
+    bool ois;
+
+public:
+    Camera() : Matrix(800, 600), pixel_size(1), ois(false) {
+        std::cout << "Camera()" << std::endl;
+    }
+
+    Camera(int matrix_width, int matrix_height,
+           float pixel_size, bool ois) : Matrix(matrix_width, matrix_height),
+                                         pixel_size(pixel_size),
+                                         ois(ois) {
+        std::cout << "Camera(int matrix_width, int matrix_height, float pixel_size, bool ois)" << std::endl;
+    }
+
+    Camera(const Camera &camera) : Matrix(camera.get_width(), camera.get_height()),
+                                   pixel_size(camera.pixel_size),
+                                   ois(camera.ois) {
+        std::cout << "Camera(const Camera &camera)" << std::endl;
+    }
+
+    bool support_ois() const { return ois; }
+
+    double get_matrix_size() {
+        return pixel_size * pixel_size * 1.0 * get_width() * get_height();
+    }
+
+    ~Camera() { std::cout << "~Camera()" << std::endl; }
+
+};
+
+
 class Phone {
 private:
     std::vector<NetworkTechnology> mobile_network_technologies;
@@ -110,20 +148,26 @@ private:
 public:
     Phone() : screen(),
               mobile_network_technologies(std::vector<NetworkTechnology>{NetworkTechnology::EDGE}) {
-        std::cout << "Phone -> Default constructor was called" << std::endl;
+        std::cout << "Phone()" << std::endl;
     }
 
-    Phone(std::vector<NetworkTechnology> mobile_network_technologies,
-          int screen_width,
-          int screen_height) : screen(screen_width, screen_height),
-                               mobile_network_technologies(mobile_network_technologies) {
-        std::cout << "Phone -> Constructor with mnt, screen_width and screen_height parameters was called" << std::endl;
+    Phone(std::vector<NetworkTechnology> mobile_network_technologies)
+            : screen(),
+              mobile_network_technologies(std::move(mobile_network_technologies)) {
+        std::cout << "Phone(std::vector<NetworkTechnology> mobile_network_technologies)" << std::endl;
     }
 
-    Phone(std::vector<NetworkTechnology> mobile_network_technologies,
-          const Screen &screen) : screen(screen),
-                                  mobile_network_technologies(mobile_network_technologies) {
-        std::cout << "Phone -> Constructor with mnt, screen_width and screen_height parameters was called" << std::endl;
+    Phone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height)
+            : screen(screen_width, screen_height),
+              mobile_network_technologies(std::move(mobile_network_technologies)) {
+        std::cout
+                << "Phone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height)"
+                << std::endl;
+    }
+
+    Phone(const Phone &phone) : screen(phone.screen),
+                                mobile_network_technologies(phone.mobile_network_technologies) {
+        std::cout << "Phone(const Phone &phone)" << std::endl;
     }
 
     const std::vector<NetworkTechnology> &get_network_technologies() const { return mobile_network_technologies; };
@@ -136,35 +180,7 @@ public:
 
     void turn_off() { working = false; }
 
-    ~Phone() { std::cout << "Phone -> The destructor was called" << std::endl; }
-};
-
-
-class Camera : public Matrix {
-private:
-    float pixel_size; // в дюймах
-    bool ois;
-
-public:
-    Camera() : Matrix(800, 600), pixel_size(1), ois(false) {
-        std::cout << "Camera -> Default contructor was called" << std::endl;
-    }
-
-    Camera(int matrix_width, int matrix_height,
-           float pixel_size, bool ois) : Matrix(matrix_width, matrix_height),
-                                         pixel_size(pixel_size),
-                                         ois(ois) {
-        std::cout << "Camera -> Constructor with args was called" << std::endl;
-    }
-
-    bool support_ois() const { return ois; }
-
-    long get_matrix_size() {
-        return pixel_size * pixel_size * get_width() * get_height();
-    }
-
-    ~Camera() { std::cout << "Camera -> The destructor was called" << std::endl; }
-
+    ~Phone() { std::cout << "~Phone()" << std::endl; }
 };
 
 
@@ -174,14 +190,18 @@ private:
 
 public:
     TouchTone() : buttons_count(12), Phone() {
-        std::cout << "TouchTone -> Default constructor was called" << std::endl;
+        std::cout << "TouchTone()" << std::endl;
     }
 
     explicit TouchTone(int buttons_count) : buttons_count(buttons_count), Phone() {
-        std::cout << "TouchTone -> Constructor with buttons_count arg was called" << std::endl;
+        std::cout << "TouchTone(int buttons_count)" << std::endl;
     }
 
-    ~TouchTone() { std::cout << "TouchTone -> The destructor was called" << std::endl; }
+    int get_buttons_count() const {
+        return buttons_count;
+    }
+
+    ~TouchTone() { std::cout << "~TouchTone()" << std::endl; }
 };
 
 
@@ -191,34 +211,44 @@ private:
     Camera camera;
 
 public:
-    Smartphone() : Phone(), rom(ROM::PixelExperience), camera() {
-        std::cout << "Smartphone -> Default constructor was called" << std::endl;
+    Smartphone() : Phone(
+            std::vector<NetworkTechnology>{
+                    NetworkTechnology::EDGE, NetworkTechnology::GPRS,
+                    NetworkTechnology::HSPA, NetworkTechnology::UMTS
+            }), rom(ROM::PixelExperience), camera() {
+        std::cout << "Smartphone()" << std::endl;
     }
 
     Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height) : Phone(
-            mobile_network_technologies, screen_width, screen_height), rom(ROM::PixelExperience), camera() {
-        std::cout << "Smartphone -> Constructor with screen size & mnt was called" << std::endl;
+            std::move(mobile_network_technologies), screen_width, screen_height), rom(ROM::PixelExperience), camera() {
+        std::cout
+                << "Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height)"
+                << std::endl;
     }
 
     Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height, ROM rom)
             : Phone(
-            mobile_network_technologies, screen_width, screen_height), rom(rom), camera() {
-        std::cout << "Smartphone -> Constructor with screen size, mnt & rom was called" << std::endl;
+            std::move(mobile_network_technologies), screen_width, screen_height), rom(rom), camera() {
+        std::cout
+                << "Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height, ROM rom)"
+                << std::endl;
     }
 
     Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height, ROM rom,
                const Camera &camera) : Phone(
-            mobile_network_technologies, screen_width, screen_height), rom(rom), camera(camera) {
-        std::cout << "Smartphone -> Constructor with screen size, mnt, rom & camera was called" << std::endl;
+            std::move(mobile_network_technologies), screen_width, screen_height), rom(rom), camera(camera) {
+        std::cout
+                << "Smartphone(std::vector<NetworkTechnology> mobile_network_technologies, int screen_width, int screen_height, ROM rom, Camera camera)"
+                << std::endl;
     }
 
-    Smartphone(const Smartphone &smartphone) : Phone(smartphone.get_network_technologies(), smartphone.get_screen()),
+    Smartphone(const Smartphone &smartphone) : Phone(smartphone),
                                                rom(smartphone.rom),
                                                camera(smartphone.camera) {
-        std::cout << "Smartphone -> The copy constructor was called" << std::endl;
+        std::cout << "Smartphone(const Smartphone &smartphone)" << std::endl;
     }
 
-    ~Smartphone() { std::cout << "Smartphone -> The destructor was called" << std::endl; }
+    ~Smartphone() { std::cout << "~Smartphone()" << std::endl; }
 
 };
 
@@ -241,7 +271,7 @@ void dynamic_test() {
     for (auto item: phonesDynamic)
         std::cout << item->is_working() << std::endl;
 
-    print_headline("Turning on phones:\n");
+    print_headline("Turning on phones...\n");
     for (auto item: phonesDynamic)
         item->turn_on();
 
@@ -266,7 +296,7 @@ void static_test() {
     for (const auto &item: phonesStatic)
         std::cout << item.is_working() << std::endl;
 
-    print_headline("Turning on phones:\n");
+    print_headline("Turning on phones...\n");
     for (auto &item: phonesStatic)
         item.turn_on();
 
